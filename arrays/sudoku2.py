f406a12a88a9a35a70209979c0ce168325aecfd0
Diego <74864968+iamdiegosanches@users.noreply.github.com>
2023-03-31 11:25:04 -0300

    Create sudoku2.py

---
+++ arrays/sudoku2.py
@@ -0,0 +1,34 @@
+# Check the rows
+# transposed_matrix = list(zip(*grid))
+# for i in range(len(transposed_matrix[0])):
+#     numeros = list(filter(lambda x: x.isdigit(), transposed_matrix[i]))
+#     if len(set(numeros)) != len(numeros):
+#         print(False)
+
+def solution(grid):
+    # Chech row
+    for row in range(9):
+        numbers = list(filter(lambda x: x.isdigit(), grid[row]))
+        if len(set(numbers)) != len(numbers):
+            return False
+    
+    # Check for column
+    for col in range(9):
+        numbers = [grid[row][col] for row in range(9) if grid[row][col].isdigit()]
+        if len(set(numbers)) != len(numbers):
+            return False
+                
+    # Check sub-grids
+    for i in range(3):
+        for j in range(3):
+            subgrid_numbers = []
+            for row in range(i*3, (i+1)*3):
+                for col in range(j*3, (j+1)*3):
+                    if grid[row][col].isdigit():
+                        subgrid_numbers.append(grid[row][col])
+            if len(set(subgrid_numbers)) != len(subgrid_numbers):
+                return False
+    
+    # Passed all the checks
+    return True
+    
