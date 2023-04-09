f143ddc2ac63616b171e24a54758fcd3543dc96a
Diego <74864968+iamdiegosanches@users.noreply.github.com>
2023-04-09 10:31:25 -0300

    Update and rename isAnagram to isAnagram.py

--- arrays/isAnagram.py
+++ arrays/isAnagram.py
@@ -8,3 +8,11 @@ class Solution:
             dS[s[i]] = 1 + dS.get(s[i], 0)
             dT[t[i]] = 1 + dT.get(t[i], 0)
         return dS == dT
+
+ # Second solution
+ class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t): return False
+
+        return ''.join(sorted(s)) == ''.join(sorted(t))
+ 
